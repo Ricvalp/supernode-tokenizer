32629fff01dd515a9520dc8695117fe48d634f7b
ricvalp <valperga.riccardo@gmail.com>
2026-04-27 12:00:24 +0200

    resume rlbench perceiver chunk low data

--- experiments/resume/exp05_rlbench18_perceiver_chunk_lowdata20.sh
+++ experiments/resume/exp05_rlbench18_perceiver_chunk_lowdata20.sh
@@ -4,7 +4,7 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
 cd "$ROOT_DIR"
 : "${SUPERNODE_TOKENIZER_CHECKPOINT_ROOT:?source an environment script that sets SUPERNODE_TOKENIZER_CHECKPOINT_ROOT}"
 RUN_NAME="rlbench18_perceiver_chunk_lowdata20"
-RESUME_PATH="${SUPERNODE_TOKENIZER_CHECKPOINT_ROOT}/${RUN_NAME}/step_0000000.pt"
+RESUME_PATH="${SUPERNODE_TOKENIZER_CHECKPOINT_ROOT}/${RUN_NAME}/step_0140000.pt"
 python scripts/train_chunk_policy.py \
   --config=configs/train_chunk_policy.py \
   --config.model.encoder_name=perceiver \
